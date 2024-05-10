69ddc4abec0859231f453094da921e020e209774
trpnayak <60289278+trpnayak@users.noreply.github.com>
2024-05-10 14:26:30 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Dummy data to store todos
+todos = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    todo = request.form.get('todo')
+    todos.append(todo)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:index>')
+def delete_todo(index):
+    del todos[index]
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
